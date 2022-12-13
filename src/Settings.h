24b2e316ff609902db9cb52624ef468c1e8615be
Gaganinja <94624203+Gaganinja@users.noreply.github.com>
2022-12-13 14:19:33 +0100

    Version fonctionnelle example + sleep

--- src/Settings.h
+++ src/Settings.h
@@ -21,7 +21,7 @@
 #define DIO3 -1                 //not used
 #define RX_EN -1                //pin for RX enable, used on some SX1280 devices, set to -1 if not used
 #define TX_EN -1                //pin for TX enable, used on some SX1280 devices, set to -1 if not used 
-
+#define BUZZER -1
 
 #define LORA_DEVICE DEVICE_SX1280                //we need to define the device we are using  
 
@@ -29,9 +29,9 @@
 const uint32_t Frequency = 2425000000;           //frequency of transmissions
 const int32_t Offset = 0;                        //offset frequency for calibration purposes
 const uint8_t Bandwidth = LORA_BW_0800;          //LoRa bandwidth
-const uint8_t SpreadingFactor = LORA_SF12;        //LoRa spreading factor
+const uint8_t SpreadingFactor = LORA_SF7;        //LoRa spreading factor
 const uint8_t CodeRate = LORA_CR_4_5;            //LoRa coding rate
 
 const int8_t TXpower = 10;                       //Power for transmissions in dBm
-
 const uint16_t packet_delay = 100;              //mS delay between packets
+#define TXBUFFER_SIZE 32
--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,141 @@
-
 /*******************************************************************************************************
-  Programs for Arduino - Copyright of the author Stuart Robinson - 06/02/20
+  Programs for Arduino - Copyright of the author Stuart Robinson - 29/02/20
 
   This program is supplied as is, it is up to the user of the program to decide if the program is
   suitable for the intended purpose and free from errors.
 *******************************************************************************************************/
 
-
 /*******************************************************************************************************
-  Program Operation - This is a simple LoRa test transmitter. A packet containing ASCII text is sent
-  according to the frequency and LoRa settings specified in the 'Settings.h' file. The pins to access
-  the lora device need to be defined in the 'Settings.h' file also.
+  Program Operation - This program tests the sleep mode and register retention of the lora device in sleep
+  mode, it assumes an Atmel ATMega328P processor is in use. The LoRa settings to use are specified in the
+  'Settings.h' file.
 
-  The details of the packet sent and any errors are shown on the Serial Monitor, together with the transmit
-  power used, the packet length and the CRC of the packet. The matching receive program, '4_LoRa_Receive'
-  can be used to check the packets are being sent correctly, the frequency and LoRa settings (in Settings.h)
-  must be the same for the Transmit and Receive program. Sample Serial Monitor output;
+  A packet is sent, containing the text 'Before Device Sleep' and the LoRa device and Atmel processor are put
+  to sleep. The processor watchdog timer should wakeup the processor in 15 seconds (approx) and register
+  values should be retained.  The device then attempts to transmit another packet 'After Device Sleep'
+  without re-loading all the LoRa settings. The receiver should see 'After Device Sleep' for the first
+  packet and 'After Device Sleep' for the second.
 
-  10dBm Packet> {packet contents*}  BytesSent,23  CRC,DAAB  TransmitTime,54mS  PacketsSent,1
+  Tested on a 'bare bones' ATmega328P board, the current in sleep mode was 12.2uA.
 
-  Serial monitor baud rate is set at 9600
+  Serial monitor baud rate is set at 9600.
 *******************************************************************************************************/
 
 #define Program_Version "V1.0"
 
-#include <SPI.h>                                               //the SX128X device is SPI based so load the SPI library                                         
-#include <SX128XLT.h>                                          //include the appropriate library  
-#include "Settings.h"                                          //include the setiings file, frequencies, LoRa settings etc   
+#include <avr/wdt.h>                 //watchdog timer library, integral to Arduino IDE
+#include <SPI.h>
+#include <LowPower.h>                //get the library here; https://github.com/rocketscream/Low-Power
 
-SX128XLT LT;                                                   //create a library class instance called LT
+#include <SX128XLT.h>
+#include "Settings.h"
 
+SX128XLT LT;
+
+bool SendOK;
+int8_t TestPower;
 uint8_t TXPacketL;
-uint32_t TXPacketCount, startmS, endmS;
 
-uint8_t buff[] = "Hello World 1234567890";
+
+void sleep1second(uint32_t sleeps)
+{
+  //uses the lowpower library
+  uint32_t index;
+
+  for (index = 1; index <= sleeps; index++)
+  {
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);         //sleep in 1 second steps
+  }
+}
+
 
 void packet_is_OK()
 {
-  //if here packet has been sent OK
-  uint16_t localCRC;
-
-  Serial.print(F("  BytesSent,"));
-  Serial.print(TXPacketL);                             //print transmitted packet length
-  localCRC = LT.CRCCCITT(buff, TXPacketL, 0xFFFF);
-  Serial.print(F("  CRC,"));
-  Serial.print(localCRC, HEX);                              //print CRC of sent packet
-  Serial.print(F("  TransmitTime,"));
-  Serial.print(endmS - startmS);                       //print transmit time of packet
-  Serial.print(F("mS"));
-  Serial.print(F("  PacketsSent,"));
-  Serial.print(TXPacketCount);                         //print total of packets sent OK
+  Serial.print(F(" "));
+  Serial.print(TXPacketL);
+  Serial.print(F(" Bytes SentOK"));
 }
 
 
 void packet_is_Error()
 {
-  //if here there was an error transmitting packet
   uint16_t IRQStatus;
-  IRQStatus = LT.readIrqStatus();                  //read the the interrupt register
-  Serial.print(F(" SendError,"));
+  IRQStatus = LT.readIrqStatus();                           //get the IRQ status
+  Serial.print(F("SendError,"));
   Serial.print(F("Length,"));
-  Serial.print(TXPacketL);                         //print transmitted packet length
+  Serial.print(TXPacketL);
   Serial.print(F(",IRQreg,"));
-  Serial.print(IRQStatus, HEX);                    //print IRQ status
-  LT.printIrqStatus();                             //prints the text of which IRQs set
+  Serial.print(IRQStatus, HEX);
+  LT.printIrqStatus();
+  digitalWrite(LED1, LOW);                                  //this leaves the LED on slightly longer for a packet error
+}
+
+
+bool Send_Test_Packet1()
+{
+  uint8_t bufffersize;
+
+  uint8_t buff[] = "Before Device Sleep";
+  TXPacketL = sizeof(buff);
+  buff[TXPacketL - 1] = '*';
+
+  if (sizeof(buff) > TXBUFFER_SIZE)              //check that defined buffer is not larger than TX_BUFFER
+  {
+    bufffersize = TXBUFFER_SIZE;
+  }
+  else
+  {
+    bufffersize = sizeof(buff);
+  }
+
+  TXPacketL = bufffersize;
+
+  LT.printASCIIPacket( (uint8_t*) buff, bufffersize);
+  digitalWrite(LED1, HIGH);
+
+  if (LT.transmit( (uint8_t*) buff, TXPacketL, 10000, TXpower, WAIT_TX))
+  {
+    digitalWrite(LED1, LOW);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+bool Send_Test_Packet2()
+{
+  uint8_t bufffersize;
+
+  uint8_t buff[] = "After Device Sleep";
+  TXPacketL = sizeof(buff);
+  buff[TXPacketL - 1] = '*';
+
+  if (sizeof(buff) > TXBUFFER_SIZE)              //check that defined buffer is not larger than TX_BUFFER
+  {
+    bufffersize = TXBUFFER_SIZE;
+  }
+  else
+  {
+    bufffersize = sizeof(buff);
+  }
+
+  TXPacketL = bufffersize;
+
+  LT.printASCIIPacket( (uint8_t*) buff, bufffersize);
+  digitalWrite(LED1, HIGH);
+
+  if (LT.transmit( (uint8_t*) buff, TXPacketL, 10000, TXpower, WAIT_TX))
+  {
+    digitalWrite(LED1, LOW);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 
@@ -82,32 +154,24 @@ void led_Flash(uint16_t flashes, uint16_t delaymS)
 
 void setup()
 {
-  pinMode(6, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(LED1, OUTPUT);                                   //setup pin as output for indicator LED
-  led_Flash(2, 125);                                       //two quick LED flashes to indicate program start
+  pinMode(LED1, OUTPUT);                        //setup pin as output for indicator LED
+  led_Flash(2, 125);                            //two quick LED flashes to indicate program start
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
-  Serial.print(F(__TIME__));
+  Serial.print(__TIME__);
   Serial.print(F(" "));
-  Serial.println(F(__DATE__));
+  Serial.println(__DATE__);
   Serial.println(F(Program_Version));
   Serial.println();
-  Serial.println(F("3_LoRa_Transmitter Starting"));
+  Serial.println(F("5_LoRa_TX_Sleep_Timed_Wakeup_Atmel Starting"));
 
   SPI.begin();
-
-  //SPI beginTranscation is normally part of library routines, but if it is disabled in library
-  //a single instance is needed here, so uncomment the program line below
-  //SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
-
-  //setup hardware pins used by device, then check if device is found
 
   if (LT.begin(NSS, NRESET, RFBUSY, DIO1, DIO2, DIO3, RX_EN, TX_EN, LORA_DEVICE))
   {
     Serial.println(F("LoRa Device found"));
-    led_Flash(2, 125);                                   //two further quick LED flashes to indicate device found
+    led_Flash(2, 125);
     delay(1000);
   }
   else
@@ -115,72 +179,62 @@ void setup()
     Serial.println(F("No device responding"));
     while (1)
     {
-      led_Flash(50, 50);                                 //long fast speed LED flash indicates device error
+      led_Flash(50, 50);                            //long fast speed flash indicates device error
     }
   }
 
-  //The function call list below shows the complete setup for the LoRa device using the information defined in the
-  //Settings.h file.
-  //The 'Setup LoRa device' list below can be replaced with a single function call;
-  //LT.setupLoRa(Frequency, Offset, SpreadingFactor, Bandwidth, CodeRate);
-
-  //***************************************************************************************************
-  //Setup LoRa device
-  //***************************************************************************************************
   LT.setupLoRa(Frequency, Offset, SpreadingFactor, Bandwidth, CodeRate);
-  // LT.setMode(MODE_STDBY_RC);
-  // LT.setRegulatorMode(USE_DCDC);
-  // LT.setPacketType(PACKET_TYPE_LORA);
-  // LT.setRfFrequency(Frequency, Offset);
-  // LT.setBufferBaseAddress(0, 0);
-  // LT.setModulationParams(SpreadingFactor, Bandwidth, CodeRate);
-  // LT.setPacketParams(12, LORA_PACKET_VARIABLE_LENGTH, 255, LORA_CRC_ON, LORA_IQ_NORMAL, 0, 0);
-  // LT.setDioIrqParams(IRQ_RADIO_ALL, (IRQ_TX_DONE + IRQ_RX_TX_TIMEOUT), 0, 0);
-  // LT.setHighSensitivity();
-  //LT.setLowPowerRX();
-  //***************************************************************************************************
 
-  Serial.println();
-  LT.printModemSettings();                               //reads and prints the configured LoRa settings, useful check
-  Serial.println();
-  LT.printOperatingSettings();                           //reads and prints the configured operating settings, useful check
-  Serial.println();
-  Serial.println();
-  LT.printRegisters(0x900, 0x9FF);                       //print contents of device registers
-  Serial.println();
-  Serial.println();
-
-  Serial.print(F("Transmitter ready"));
+  Serial.print(F("Transmitter ready - TXBUFFER_SIZE "));
+  Serial.println(TXBUFFER_SIZE);
   Serial.println();
 }
 
-
 void loop()
 {
-  Serial.print(TXpower);                                       //print the transmit power defined
+  digitalWrite(LED1, HIGH);
+  Serial.print(TXpower);
   Serial.print(F("dBm "));
-  Serial.print(F("Packet> "));
+  Serial.print(F("TestPacket1> "));
   Serial.flush();
 
-  TXPacketL = sizeof(buff);                                    //set TXPacketL to length of array
-  buff[TXPacketL - 1] = '*';                                   //replace null character at buffer end so its visible on reciver
-
-  LT.printASCIIPacket(buff, TXPacketL);                        //print the buffer (the sent packet) as ASCII
-
-  digitalWrite(LED1, HIGH);
-  startmS =  millis();                                         //start transmit timer
-  if (LT.transmit(buff, TXPacketL, 1000, TXpower, WAIT_TX))   //will return packet length sent if OK, otherwise 0 if transmit, timeout 10 seconds
+  if (Send_Test_Packet1())
   {
-    endmS = millis();                                          //packet sent, note end time
-    TXPacketCount++;
     packet_is_OK();
   }
   else
   {
-    packet_is_Error();                                 //transmit packet returned 0, there was an error
+    packet_is_Error();
   }
-
-  digitalWrite(LED1, LOW);
   Serial.println();
-  delay(packet_delay);                                 //have a delay between packets
+  delay(packet_delay);
+
+  LT.setSleep(CONFIGURATION_RETENTION);                        //preserve register settings in sleep.
+  Serial.println(F("Sleeping zzzzz...."));
+  Serial.println();
+  Serial.flush();
+  digitalWrite(LED1, LOW);
+
+  sleep1second(15);                                            //goto sleep for 15 seconds
+
+  Serial.println(F("Awake !"));
+  Serial.flush();
+  digitalWrite(LED1, HIGH);
+  LT.wake();
+
+  Serial.print(TXpower);
+  Serial.print(F("dBm "));
+  Serial.print(F("TestPacket2> "));
+  Serial.flush();
+
+  if (Send_Test_Packet2())
+  {
+    packet_is_OK();
+  }
+  else
+  {
+    packet_is_Error();
+  }
+  Serial.println();
+  delay(packet_delay);
 }
